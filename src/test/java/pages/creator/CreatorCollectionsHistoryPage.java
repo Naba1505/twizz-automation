09179bfb3dc6e7d27a1c515d89108b0f17c448fc
Naba Teja <teja2311@gmail.com>
2026-06-30 17:35:37 +0530

    Fix CreatorCollectionsHistoryPage: add navigateAndWait for settings icon

--- src/test/java/pages/creator/CreatorCollectionsHistoryPage.java
+++ src/test/java/pages/creator/CreatorCollectionsHistoryPage.java
@@ -50,6 +50,8 @@ public class CreatorCollectionsHistoryPage extends BasePage {
     // ---------- Steps ----------
     @Step("Open Settings from profile (Collections History)")
     public void openSettingsFromProfile() {
+        // Ensure we are on the profile page before looking for the settings icon
+        navigateAndWait(ConfigReader.getBaseUrl() + "/creator/profile");
         waitVisible(settingsIcon(), ConfigReader.getShortTimeout());
         clickWithRetry(settingsIcon(), 1, ConfigReader.getElementRetryDelay());
         page.waitForURL("**" + SETTINGS_URL_PART + "**");
